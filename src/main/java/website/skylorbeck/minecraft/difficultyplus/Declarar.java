da1336e733ff55667e8915454a3808dabd0d4b54
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-12-13 15:56:53 -0800

    random spawns

--- src/main/java/website/skylorbeck/minecraft/difficultyplus/Declarar.java
+++ src/main/java/website/skylorbeck/minecraft/difficultyplus/Declarar.java
@@ -1,7 +1,13 @@
 package website.skylorbeck.minecraft.difficultyplus;
 
 import net.fabricmc.tinyremapper.extension.mixin.common.Logger;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
+
+import static net.minecraft.entity.effect.StatusEffects.*;
 
 public class Declarar {
         public static Logger logger = new Logger(Logger.Level.INFO);
@@ -10,7 +16,12 @@ public class Declarar {
                 return new Identifier(MODID,string);
         }
         public static float playerInfluence = 1f;
-        public static float xpInfluence = 0.001f;
+        public static float xpInfluence = 0.00001f;
         public static float chanceCap = 1f;
-
+        public static StatusEffect[] statusEffects = new StatusEffect[]{REGENERATION,SPEED,FIRE_RESISTANCE,STRENGTH,INVISIBILITY,WEAKNESS,SLOWNESS};
+        public static Item[] helmets = new Item[]{Items.CHAINMAIL_HELMET,Items.LEATHER_HELMET,Items.IRON_HELMET,Items.DIAMOND_HELMET};
+        public static Item[] chests = new Item[]{Items.CHAINMAIL_CHESTPLATE,Items.LEATHER_CHESTPLATE,Items.IRON_CHESTPLATE,Items.DIAMOND_CHESTPLATE};
+        public static Item[] pants = new Item[]{Items.CHAINMAIL_LEGGINGS,Items.LEATHER_LEGGINGS,Items.IRON_LEGGINGS,Items.DIAMOND_LEGGINGS};
+        public static Item[] boots = new Item[]{Items.CHAINMAIL_BOOTS,Items.LEATHER_BOOTS,Items.IRON_BOOTS,Items.DIAMOND_BOOTS};
+        public static Item[] weapons = new Item[]{Items.WOODEN_AXE,Items.WOODEN_SHOVEL,Items.WOODEN_SWORD,Items.IRON_AXE,Items.IRON_SHOVEL,Items.IRON_SWORD,Items.DIAMOND_AXE,Items.DIAMOND_SHOVEL,Items.DIAMOND_SWORD,};
 }
--- src/main/java/website/skylorbeck/minecraft/difficultyplus/mixin/SpawnHelperMixin.java
+++ src/main/java/website/skylorbeck/minecraft/difficultyplus/mixin/SpawnHelperMixin.java
@@ -1,9 +1,17 @@
 package website.skylorbeck.minecraft.difficultyplus.mixin;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.tag.Tag;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.SpawnHelper;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
@@ -21,15 +29,47 @@ import website.skylorbeck.minecraft.difficultyplus.cardinal.XPTracker;
 
 @Mixin(SpawnHelper.class)
 public class SpawnHelperMixin {
-
-    @Shadow @Final private static Logger LOGGER;
-
-    @Inject(method = "createMob", at = @At("RETURN"))
+    @Inject(method = "createMob", at = @At("RETURN"), cancellable = true)
     private static void checkDifficultSpawn(ServerWorld world, EntityType<?> type, CallbackInfoReturnable<@Nullable MobEntity> cir) {
         if (!type.getSpawnGroup().isPeaceful()) {
             XPTracker tracker = DifficultyPlusCardinal.WorldXP.get(world);
+            float chance = MathHelper.clamp((tracker.getTotalXP()*Declarar.xpInfluence)* (world.getPlayers().size()*Declarar.playerInfluence),0,Declarar.chanceCap);
+            float random= world.random.nextFloat();
+//            Declarar.logger.info(random +" "+ chance);
+            if (random<chance) {
+                MobEntity entity = cir.getReturnValue();
+                assert entity != null;
 
-            Declarar.logger.info("HOSTILESPAWN: "+(tracker.getTotalXP()*Declarar.xpInfluence)+" "+ world.getPlayers().size()*Declarar.playerInfluence);
+                entity.addStatusEffect(new StatusEffectInstance(Declarar.statusEffects[world.random.nextInt(Declarar.statusEffects.length)],9999,world.random.nextBoolean()?1:0));
+
+                random = world.random.nextFloat();
+                if (random<chance) {
+                    entity.equipStack(EquipmentSlot.MAINHAND, Declarar.weapons[world.random.nextInt(Declarar.weapons.length)].getDefaultStack());
+                }
+                random = world.random.nextFloat();
+                if (random<chance)
+                    entity.equipStack(EquipmentSlot.HEAD,Declarar.helmets[world.random.nextInt(Declarar.helmets.length)].getDefaultStack());
+
+                random = world.random.nextFloat();
+                if (random<chance)
+                    entity.equipStack(EquipmentSlot.CHEST,Declarar.chests[world.random.nextInt(Declarar.chests.length)].getDefaultStack());
+
+                random = world.random.nextFloat();
+                if (random<chance)
+                    entity.equipStack(EquipmentSlot.LEGS,Declarar.pants[world.random.nextInt(Declarar.pants.length)].getDefaultStack());
+
+                random = world.random.nextFloat();
+                if (random<chance)
+                    entity.equipStack(EquipmentSlot.FEET,Declarar.boots[world.random.nextInt(Declarar.boots.length)].getDefaultStack());
+
+                for (ItemStack itemStack:entity.getItemsEquipped()) {
+                    random = world.random.nextFloat();
+                    if (random < chance)
+                        EnchantmentHelper.enchant(world.random,itemStack,1,true);
+                }
+                cir.setReturnValue(entity);
+//                Declarar.logger.info("mob Upgraded");
+            }
         }
 
     }
